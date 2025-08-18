565638e640041a26d24fd08ca13fdc6404170a89
Pravin Mishra <pravinmishra000@gmail.com>
2025-08-18 04:35:01 +0000

    Set the Home Page background with a vertical gradient: - Top 20% → Vib

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,10 @@ export default {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  safelist: [
+    'from-orange-400',
+    'to-background'
+  ],
   theme: {
     extend: {
       fontFamily: {
